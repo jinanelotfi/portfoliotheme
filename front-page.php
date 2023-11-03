9e90b4a487e9be92d5f07368a331f301866ebd58
Jinane Lotfi <jinane.lofi@gmail.com>
2023-11-03 17:06:11 +0100

    Design page home ok + mise en place page contact

--- front-page.php
+++ front-page.php
@@ -4,13 +4,17 @@
     <div class="home-container">
         <article class="projets">
             <div class="myself-section">
+                <p>Developpeuse WEB</p>
                 <h1>Jinane Lotfi</h1>
-                <p>je suis une web designer basée à san francisco</p>
-                <div class="btn-card">
-                    <a href="<?php the_permalink(); ?>" class="button">
-                        <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                    </a>
-                </div> 
+                <div class="description-self-container">
+                    <p>Développeuse Front-end spécialisée dans la création de sites web Wordpress. Ma mission consiste à concevoir pour vous des sites fonctionnels et esthétiques.</p>
+                    <div class="btn-card">
+                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>" class="button">
+                            <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                        </a>
+                    </div> 
+                </div>
+                
             </div>
             <div class="line-projets-about-container">
                 <div class="scrolling-text-container">
@@ -19,23 +23,32 @@
                 <div class="projets-about-section">                    
                     <div class="about-section">
                         <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/sign.png'; ?>" alt="signature">
-                        <p>Plus à propos de moi</p>
-                        <h2>Parcourt</h2>
-                        <div class="btn-card">
-                            <a href="<?php the_permalink(); ?>" class="button">
-                                <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                            </a>
-                        </div>                         
+                        <div class="side-card">
+                            <span>
+                                <p>Plus à propos de moi</p>
+                                <h2>Parcourt</h2>
+                            </span>                            
+                            <div class="btn-card">
+                                <a href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>" class="button">
+                                    <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                                </a>
+
+                            </div>   
+                        </div>                      
                     </div>
-                    <div class="projets-section">
-                        <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/my-works.png'; ?>" alt="pc">
-                        <p>Portfolio</p>
-                        <h2>Projets</h2>
-                        <div class="btn-card">
-                            <a href="<?php the_permalink(); ?>" class="button">
-                                <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                            </a>
-                        </div> 
+                    <div class="projet-section">
+                        <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/my-works.png'; ?>" alt="signature">
+                        <div class="side-card">
+                            <span>
+                                <p>Mes projets</p>
+                                <h2>Portfolio</h2>
+                            </span>                            
+                            <div class="btn-card">
+                                <a href="<?php echo esc_url(get_permalink(get_page_by_path('portfolio'))); ?>" class="button">
+                                    <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                                </a>
+                            </div>   
+                        </div>                      
                     </div>
 
 
@@ -45,48 +58,76 @@
         </article>
         <article class="services-container">
             <div class="cv-competences-section">
-                <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/gfonts.png'; ?>" alt="gglfonts">
-                <p>PDF</p>
-                <h2>Mon CV</h2>
-                <div class="btn-card">
-                    <a href="<?php the_permalink(); ?>" class="button">
-                        <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                    </a>
-                </div> 
+                <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/sign.png'; ?>" alt="signature">
+                <div class="side-card">
+                    <span>
+                        <p>PDF</p>
+                        <h2>Mon CV à télécharger</h2>
+                    </span>                            
+                    <div class="btn-card">
+                        <a href="<?php echo esc_url(get_template_directory_uri() . './assets/uploads/cv-jinane-digital.pdf'); ?>" class="button" download>
+                            <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                        </a>
+                    </div>   
+                </div>                      
             </div>
-            <div class="social-section">
-                <p>Compétences</p>
-                <h2>Offres de service</h2>
-                <div class="btn-card">
-                    <a href="<?php the_permalink(); ?>" class="button">
-                        <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                    </a>
-                </div> 
 
+            <div class="competences-section">
+                <span class="competences-icones">
+                    <i class="fa-brands fa-square-js" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-html5" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-css3-alt" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-php" style="color: #ffffff;"></i>                
+                </span>
+                <div class="side-card">
+                    <span>
+                        <p>compétences</p>
+                        <h2>Offres de service</h2>
+                    </span>                            
+                    <div class="btn-card">
+                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>" class="button">
+                            <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                        </a>
+                    </div>   
+                </div>                      
             </div>
+
             <div class="contact-section">
-                <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/my-works.png'; ?>" alt="pc">
-                <p>On reste en contact</p>
-                <h2>Mes réseaux</h2>
-                <div class="btn-card">
-                    <a href="<?php the_permalink(); ?>" class="button">
-                        <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                    </a>
-                </div> 
-
+                <span class="image-social">
+                    <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/linkedin.svg'; ?>" alt="linkedin">
+                    <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/github.svg'; ?>" alt="github">
+                </span>                
+                <div class="side-card">
+                    <span>
+                        <p>Mes réseaux</p>
+                        <h2>On reste en contact</h2>
+                    </span>                            
+                    <div class="btn-card">
+                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="button">
+                            <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                        </a>
+                    </div>   
+                </div>                      
             </div>
         </article>
-        <article class="contact-container">            
-            <div class="contact-go-section">
-                <img class="image-sign" src="<?php echo get_template_directory_uri() . './assets/images/my-works.png'; ?>" alt="pc">
-                <h2>Travaillons ensembles</h2>
-                <div class="btn-card">
-                    <a href="<?php the_permalink(); ?>" class="button">
-                        <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
-                    </a>
-                </div> 
-
+        <article class="contact-container">           
+            <div class="contact-go-section">                
+                <div class="side-card">
+                    <span>
+                        <h2>Etes-vous prêt<br>pour travailler <span>ensemble ?</span>
+                        </h2>
+                    </span>                            
+                    <div class="btn-card">
+                        <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="button">
+                            <i class="fa-regular fa-eye" style="color: #ffffff;"></i>
+                        </a>
+                    </div>   
+                </div>                      
             </div>
+
+
+
+
         </article>
 
 
--- page-contact.php
+++ page-contact.php
@@ -4,16 +4,40 @@
 get_header();
 ?>
 
-<main class="contact-container">
+<main class="page-contact-container">
     <div class="side-contact">
         <div class="contact-icones">
-            <h2></h2>
-            <div class="mail"></div>
-            <div div class="tel"></div>
-            <div class="location"></div>
+            <h2>Informations de contact</h2>
+            <div class="mail">
+                <span class="span-i">
+                    <i class="fa-solid fa-envelope" style="color: #ffffff;"></i>
+                </span>                
+                <span class="span">
+                    <h3>Mail</h3>
+                    <p>jinane.lofi@gmail.com</p>
+                </span>
+            </div>
+            <div class="tel">
+                <span class="span-i">
+                    <i class="fa-solid fa-phone" style="color: #ffffff;"></i>
+                </span>
+                <span class="span">
+                    <h3>Téléphone</h3>
+                    <p>+33 6 36 11 79 45</p>
+                </span>
+            </div>
+            <div class="location">
+                <span class="span-i">
+                    <i class="fa-solid fa-location-dot" style="color: #ffffff;"></i>
+                </span>
+                <span class="span">
+                    <h3>Localisation</h3>
+                    <p>Lyon</p>
+                </span>
+            </div>
         </div>
         <div class="social-info">
-            <h2></h2>
+            <h2>Réseaux sociaux</h2>
             <img src="" alt="">
             <img src="" alt="">
             <img src="" alt="">
@@ -23,6 +47,7 @@ get_header();
         <!-- Modal content -->
         <div class="box">                  
             <div class="contact-box">
+                <h2>Travaillons ensemble</h2>
                 <?php echo do_shortcode('[contact-form-7 id="0e27346" title="Contact"]'); ?>
             </div>           
         </div>
