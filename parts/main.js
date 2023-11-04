3b0b74344fc02047ca16a37e9e0dd64fb9883ccb
Jinane Lotfi <jinane.lofi@gmail.com>
2023-11-04 14:28:43 +0100

    Ajout responsive de la page contact ok

--- parts/main.js
+++ parts/main.js
@@ -1,16 +1,16 @@
 // Réglages appartition des titres
 // On crée l'observateur
-// const observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         // Si l'élément est visible, on ajoute la classe d'animation
-//         entry.target.classList.add('appear');
-//       } else {
-//         // Si l'élément n'est pas visible, on retire la classe d'animation
-//         entry.target.classList.remove('appear');
-//       }
-//     });
-// });
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Si l'élément est visible, on ajoute la classe d'animation
+        entry.target.classList.add('appear');
+      } else {
+        // Si l'élément n'est pas visible, on retire la classe d'animation
+        entry.target.classList.remove('appear');
+      }
+    });
+});
 
 // On récupère tous les éléments ayant la classe pin et on commence à les observer
 const pinElements = document.querySelectorAll('.pin');
